76b72bfae29f138f9a2c8409628dd01b54cbaa71
Sembler200 <61061627+Sembler200@users.noreply.github.com>
2022-03-11 23:18:45 -0500

    Import galilee, Base map

---
+++ WowSpring22/Assets/Jake'sFolder/MapsforMapScene/README.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class README : MonoBehaviour
+{
+
+    /* The dimensions of terrain plot is 
+     * W = 6912
+     * L = 6912
+     * H ~ 1250 
+     
+
+
+
+
+}
