375771ec6395efe292448ad4332e5a635629d9a1
Sembler200 <61061627+Sembler200@users.noreply.github.com>
2022-03-13 17:28:09 -0400

    Cinema machine Dolly Track
    The beginnings of the zoom in effect

--- WowSpring22/Assets/Jake'sFolder/MapsforMapScene/README.cs
+++ WowSpring22/Assets/Jake'sFolder/MapsforMapScene/README.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class README : MonoBehaviour
-{
+{ }
 
     /* The dimensions of terrain plot is 
      * W = 6912
      * L = 6912
      * H ~ 1250 
-     
+     */
 
 
 
 
-}
+
