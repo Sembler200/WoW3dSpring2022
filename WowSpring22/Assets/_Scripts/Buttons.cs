0f7027ca214e87a938eed1a96d8045c03b28bd5d
Eric Laputka <elaputka34@gmail.com>
2022-03-24 10:17:42 -0400

    PreAlpha Work-Eric

---
+++ WowSpring22/Assets/_Scripts/Buttons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    public SceneTransitions st;
+
+    public void ToHula()
+    {
+        st.toScene = "Hula";
+        st.GoTo();
+       // FindObjectOfType<SceneController>().LoadScene("Hula");
+    }
+
+    public void ToGalilee()
+    {
+        FindObjectOfType<SceneController>().LoadScene("Galilee");
+    }
+
+
+}
---
+++ WowSpring22/Assets/_Scripts/SceneTransitions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransitions : MonoBehaviour
+{
+    public Animator transition;
+    public float sec;
+    public SceneController sc;
+    public string toScene;
+
+    public Camera cam;
+
+    public void GoTo()
+    {
+
+        StartCoroutine(Transition());
+    }
+    public IEnumerator Transition()
+    {
+        //transition.SetTrigger("Start");
+        cam.fieldOfView += 30;
+        yield return new WaitForSeconds(sec);
+        sc.LoadScene(toScene);
+    }
+}
