67b14c5951bb898c12994808d760c07f1f258789
Eric <elaputka34@gmail.com>
2022-04-12 00:12:32 -0400

    Painted Terrains, Time Travel for main map and Gal scenes

---
+++ WowSpring22/Assets/_Scripts/CivTimeTravel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Playables;
+
+
+
+public class CivTimeTravel : MonoBehaviour
+{
+    public Scrollbar sb;
+    public PlayableDirector pb;
+    public float actTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pb.playableGraph.GetRootPlayable(0).SetSpeed(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetTime()
+    {
+        Debug.Log(sb.value);
+        float maxtime = 1;
+
+        actTime = sb.value * maxtime;
+
+        pb.time = actTime;
+        pb.RebuildGraph();
+        pb.Play();
+        pb.playableGraph.GetRootPlayable(0).SetSpeed(0);
+    }
+}
